0ab41d0d3ee211335a807cb95ba29cfd87caba9c
Kasthuri-5A3 <kasthuri4536@gmail.com>
2026-02-07 11:12:30 +0530

    Add files via upload

---
+++ PrimitiveDataType.java
@@ -0,0 +1,17 @@
+
+
+public class PrimitiveDataType {
+    public static void main(String[] args){
+        String name="latha";
+        int a=18;
+        char ch='A';
+        int b=956;
+        boolean result=true;
+        System.out.println("Student name:"+name);
+        System.out.println("Age:"+a);
+        System.out.println("Grade:"+ch);
+        System.out.println("b:"+b);
+        System.out.println("result:"+result);
+    }
+    
+}
