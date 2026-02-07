febecc20b59d44c5abb8e5181b964843238da7cb
Kasthuri-5A3 <kasthuri4536@gmail.com>
2026-02-07 14:05:23 +0530

    Add files via upload

---
+++ ArithmeticOperator.java
@@ -0,0 +1,11 @@
+public class ArithmeticOperator {
+    public static void main(String[] args){
+        int a=9;
+        int b=5;
+        System.out.println("sum:"+(a+b));
+        System.out.println("sub:"+(a-b));
+        System.out.println("mul:"+(a*b));
+        System.out.println("div:"+(a/b));
+        System.out.println("mod:"+(a%b));
+}
+}
